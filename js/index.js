49ffc20d8bdfa9346fd8896131ba0a43c26e3f80
Joseph carabajo <joemcara@espol.edu.ec>
2023-06-29 18:12:49 -0800

    version 1.1.0

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ let plot = (data) => {
     const ctx = document.getElementById('myChart')
 
     const dataset = {
-        labels: data.hourly.time, /* ETIQUETA DE DATOS */
+        labels: data.hourly.time,
         datasets: [{
-            label: 'Temperatura semanal', /* ETIQUETA DEL GRÁFICO */
-            data: data.hourly.temperature_2m, /* ARREGLO DE DATOS */
+            label: 'Temperatura semanal',
+            data: data.hourly.temperature_2m,
             fill: false,
             borderColor: 'rgb(75, 192, 192)',
             tension: 0.1
@@ -21,35 +21,68 @@ let plot = (data) => {
     const chart = new Chart(ctx, config)
 }
 
-let load = (data) =>{
-    console.log(data)
-    plot(data)
+let bar = (data) =>{
+    const ctx = document.getElementById('myChart2')
+
+    const dataset = {
+        labels: data.daily.time,
+
+        datasets: [{
+            label: 'Temperatura semanal',
+            data: data.daily.uv_index_max,
+            fill: false,
+            backgroundColor: [
+                'rgba(54, 162, 235, 2)',
+                'rgba(255, 99, 132, 2)',
+                'rgba(255, 99, 132, 2)',
+                'rgba(255, 99, 132, 2)',
+                'rgba(54, 162, 235, 2)',
+                'rgba(75, 192, 192, 2)',
+                'rgba(75, 192, 192, 2)'
+
+            ],
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    };
+
+    const config = {
+        type: 'bar',
+        data: dataset,
+    };
+
+    const chart = new Chart(ctx, config)
 }
 
 
-(
-    function () {
-        let url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&daily=uv_index_max&timezone=auto'
-        fetch(url)
-            .then(response => response.json())
-                let meteo = localStorage.getItem('meteo');
-                if(meteo == null) {
-                    let URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&daily=uv_index_max&timezone=auto';
-                        
-                    fetch(URL)
-                    .then(response => response.json())
-                    .then(data => {
-                        load(data)
-                
-                        /* GUARDAR DATA EN LA MEMORIA */
-                        localStorage.setItem('meteo',JSON.stringify(data))
-                    })
-                    .catch(console.error);
-                
-                  } else {
-                
-                      /* CARGAR DATA DESDE LA MEMORIA */
-                      load(JSON.parse(meteo))  
-                  }
-    }
-)();
+let load = (data) => {
+    document.getElementById('timeZone').innerHTML = data['timezone']
+    document.getElementById('elevation').innerHTML = data['elevation'] + '%'
+    document.getElementById('latitude').innerHTML = data['latitude']
+    plot(data)
+    bar(data)
+}
+
+    (
+        function () {
+            let meteo = localStorage.getItem('meteo');
+            if(meteo == null) {
+                let URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&daily=uv_index_max&timezone=auto';
+                    
+                fetch(URL)
+                .then(response => response.json())
+                .then(data => {
+                    load(data)
+            
+                    /* GUARDAR DATA EN LA MEMORIA */
+                    localStorage.setItem("meteo", JSON.stringify(data))
+                })
+                .catch(console.error);
+            
+              } else {
+            
+                  /* CARGAR DATA DESDE LA MEMORIA */
+                  load(JSON.parse(meteo))
+              }
+        }
+    )();
